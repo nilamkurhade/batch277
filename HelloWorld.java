b9fd95ca117923710e62bc024afaf1160da8e47f
nilamkurhade <nilamkurhade87@gmail.com>
2021-03-15 18:53:02 +0530

    dsy6 programs

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld
+{
+	public static void main(String args[])
+	{
+		while(true)
+		{
+			System.out.println("Hello world");
+			try
+			{
+				Thread.sleep(6000);
+			}
+			catch(Exception e)
+			{
+				System.out.println(e);
+			}
+		}
+	}
+}
